c558ec1288b9e8b9a0e00a8ab32098f00c422309
Imran-Qumtur <Imran-Qumtur@users.noreply.github.com>
2018-04-19 23:50:17 +0200

    Unity spel

---
+++ MobileJoystickTest/Assets/Scripts/PlayerMovment.cs
@@ -0,0 +1,142 @@
+﻿using UnityEngine;
+using System.Collections;
+public class PlayerMovment : MonoBehaviour
+{
+    public float moveSpeed = 05f;
+    public VJHandler jsMovement;
+    private Vector3 direction;
+    public Sprite NORTHWEST, NORTH, NORTHEAST, WEST, EAST, SOUTHWEST, SOUTH, SOUTHEAST;
+    public GameObject hitArea;
+    private int facingDirection;
+    public Animation attackAnimation;
+
+
+
+
+
+    void Update()
+    {
+
+        float oldX = transform.position.x;
+        float oldY = transform.position.y;
+
+
+        direction = jsMovement.InputDirection; //InputDirection can be used as per the need of your project
+
+        if (direction.magnitude != 0)
+        {
+            transform.position += direction * moveSpeed;
+            jsMovement.angle = Mathf.Atan2((transform.position.x - oldX), (transform.position.y - oldY)) * Mathf.Rad2Deg;
+            Debug.Log(jsMovement.angle);    
+            if (jsMovement.angle > -67 && jsMovement.angle < -22)
+            {
+                Debug.Log("NorthWest");
+                this.GetComponent<SpriteRenderer>().sprite = NORTHWEST;
+                switchArea(1);
+            }       
+         
+            else if (jsMovement.angle > -22 && jsMovement.angle < 22)
+            {
+                Debug.Log("North");
+                this.GetComponent<SpriteRenderer>().sprite = NORTH;
+                switchArea(2);
+            }
+            else if (jsMovement.angle > 22 && jsMovement.angle < 67)
+            {
+                Debug.Log("NorthEast");
+                this.GetComponent<SpriteRenderer>().sprite = NORTHEAST;
+                switchArea(3);
+            }
+            else if (jsMovement.angle > -112 && jsMovement.angle < -67)
+            {
+                Debug.Log("West");
+                this.GetComponent<SpriteRenderer>().sprite = WEST;
+                switchArea(4);
+            }
+            else if (jsMovement.angle > 67 && jsMovement.angle < 112)
+            {
+                Debug.Log("East");
+                this.GetComponent<SpriteRenderer>().sprite = EAST;
+                switchArea(5);
+            }
+            else if (jsMovement.angle > -157 && jsMovement.angle < -112)
+            {
+                Debug.Log("SouthWest");
+                this.GetComponent<SpriteRenderer>().sprite = SOUTHWEST;
+                switchArea(6);
+            }
+            else if (jsMovement.angle > 157 || jsMovement.angle <= (-157))
+            {
+                Debug.Log("South");
+                this.GetComponent<SpriteRenderer>().sprite = SOUTH;
+                switchArea(7);
+            }
+            else if (jsMovement.angle > 112 && jsMovement.angle < 156)
+            {
+                Debug.Log("SouthEast");
+                this.GetComponent<SpriteRenderer>().sprite = SOUTHEAST;
+                switchArea(8);
+            }
+        }
+
+    }
+    public void switchArea(int direction)
+    {
+        switch (direction)
+        {
+            case 1:
+                hitArea.transform.localPosition = new Vector3(-0.1f, 0.1f);
+                hitArea.transform.localRotation = Quaternion.Euler(new Vector3(0, 0, -135));
+                break;
+            case 2:
+                hitArea.transform.localPosition = new Vector3(0, 0.1f);
+                hitArea.transform.localRotation = Quaternion.Euler(new Vector3(0, 0, -180));
+                break;
+            case 3:
+                hitArea.transform.localPosition = new Vector3(0.1f, 0.1f);
+                hitArea.transform.localRotation = Quaternion.Euler(new Vector3(0, 0, 135));
+                break;
+            case 4:
+                hitArea.transform.localPosition = new Vector3(-0.1f,0);
+                hitArea.transform.localRotation = Quaternion.Euler(new Vector3(0, 0, -90));
+
+                break;
+            case 5:
+                hitArea.transform.localPosition = new Vector3(0.1f, 0);
+                hitArea.transform.localRotation = Quaternion.Euler(new Vector3(0, 0, 90));
+
+                break;
+            case 6:
+                hitArea.transform.localPosition = new Vector3(-0.1f, -0.1f);
+                hitArea.transform.localRotation = Quaternion.Euler(new Vector3(0, 0, -45));
+
+                break;
+            case 7:
+                hitArea.transform.localPosition = new Vector3(0, -0.1f);
+                hitArea.transform.localRotation = Quaternion.Euler(new Vector3(0, 0, 0));
+          
+                break;
+            case 8:
+                hitArea.transform.localPosition = new Vector3(0.1f, -0.1f);
+                hitArea.transform.localRotation = Quaternion.Euler(new Vector3(0, 0, 45));
+
+                break;
+
+        }
+            
+    }
+    public void dash()
+    {
+        transform.position += direction * 2;
+        Debug.Log("Hello");
+    }
+    public void attack()
+    {
+        attackAnimation.wrapMode = WrapMode.Once;
+        attackAnimation.Play();
+    }
+    void Start()
+    {
+        this.GetComponent<SpriteRenderer>().sprite = SOUTH;
+    }
+}
